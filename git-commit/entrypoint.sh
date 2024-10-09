888fd0440399461c119dfc25d8bda6d291b79606
Lukas Jenicek <lj@horizon.io>
2024-10-09 15:27:33 +0200

    pr create make boolean

--- git-commit/entrypoint.sh
+++ git-commit/entrypoint.sh
@@ -23,9 +23,9 @@ then
   INPUT_PR_DESCRIPTION="Triggered by https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
 fi
 
-if [ -z "$INPUT_DST_BRANCH" ]
+if [ -z "$INPUT_BRANCH" ]
 then
-  INPUT_DST_BRANCH=${GITHUB_HEAD_REF}
+  INPUT_BRANCH=${GITHUB_HEAD_REF}
 fi
 
 if [ -z "$INPUT_USER_NAME" ]
@@ -38,6 +38,11 @@ then
   INPUT_USER_EMAIL="$GITHUB_ACTOR_ID+$GITHUB_ACTOR@users.noreply.github.com"
 fi
 
+if [ -z "$INPUT_PR_CREATE" ]
+then
+  INPUT_PR_CREATE=false
+fi
+
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
@@ -48,9 +53,9 @@ git clone "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_REPOSITORY
 BASE_DIR=$(pwd)
 cd "$CLONE_DIR"
 
-echo "Creating new branch: ${INPUT_DST_BRANCH}"
-git checkout -b "$INPUT_DST_BRANCH"
-git reset --hard "origin/$INPUT_DST_BRANCH"  || true
+echo "Creating new branch: ${INPUT_BRANCH}"
+git checkout -b "$INPUT_BRANCH"
+git reset --hard "origin/$INPUT_BRANCH"  || true
 
 DEST_COPY="$CLONE_DIR/$INPUT_DST"
 if [ "$INPUT_DST" != "" ]
@@ -67,16 +72,16 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "${INPUT_COMMIT_MESSAGE}"
   echo "Pushing git commit"
-  git push -u origin HEAD:"$INPUT_DST_BRANCH"
+  git push -u origin HEAD:"$INPUT_BRANCH"
 
-  if [ "$INPUT_PR_CREATE" == "true" ]
+  if [ "$INPUT_PR_CREATE" == true ]
   then
     PR_DESCRIPTION_ESCAPED="${INPUT_PR_DESCRIPTION//$'\n'/\\n}"
 
     curl --connect-timeout 10 \
       -u "${INPUT_USER_NAME}:${API_TOKEN_GITHUB}" \
       -X POST -H 'Content-Type: application/json' \
-      --data "{\"head\":\"$INPUT_DST_BRANCH\",\"base\":\"${INPUT_PR_BASE}\", \"title\": \"${INPUT_PR_TITLE}\", \"body\": \"${PR_DESCRIPTION_ESCAPED}\"}" \
+      --data "{\"head\":\"$INPUT_BRANCH\",\"base\":\"${INPUT_PR_BASE}\", \"title\": \"${INPUT_PR_TITLE}\", \"body\": \"${PR_DESCRIPTION_ESCAPED}\"}" \
       "https://api.github.com/repos/{$INPUT_REPOSITORY}/pulls"
   fi
 else
